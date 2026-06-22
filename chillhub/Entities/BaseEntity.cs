eefd0b9a57e596c36cd4e2a58409c30a604634dc
jukistricker <thanhthanhbg1@gmail.com>
2026-06-22 12:05:29 +0700

    stagged

--- chillhub/Entities/BaseEntity.cs
+++ chillhub/Entities/BaseEntity.cs
@@ -3,7 +3,7 @@
     public interface IAuditEntity
     {
         DateTimeOffset CreatedAt { get; set; }
-        DateTimeOffset UpdatedAt { get; set; }
+        DateTimeOffset? UpdatedAt { get; set; }
     }
     
     public abstract class BaseEntity<T> : IAuditEntity
@@ -11,7 +11,7 @@
         public T Id { get; set; }
         public DateTimeOffset CreatedAt { get; set; } 
         public T? CreatedBy { get; set; }
-        public DateTimeOffset UpdatedAt { get; set; }
+        public DateTimeOffset? UpdatedAt { get; set; }
         public T? UpdatedBy { get; set; }
     }
     
@@ -20,7 +20,7 @@
         public Guid Id { get; set; }
         public DateTimeOffset CreatedAt { get; set; }
         public Guid? CreatedBy { get; set; }
-        public DateTimeOffset UpdatedAt { get; set; }
+        public DateTimeOffset? UpdatedAt { get; set; }
         public Guid? UpdatedBy { get; set; }
     }
 }
